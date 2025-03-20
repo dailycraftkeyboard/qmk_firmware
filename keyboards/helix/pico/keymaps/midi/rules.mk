156da3dbf09026aa150e8d2a7802b581bf38efd9
yfuku <fukumoto@gmail.com>
2025-03-20 11:36:25 +0900

    add midi keymap

---
+++ keyboards/helix/pico/keymaps/midi/rules.mk
@@ -0,0 +1,4 @@
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+AUDIO_ENABLE = yes          # Audio output
+MIDI_ENABLE = yes           # MIDI support
+LTO_ENABLE = no  # if firmware size over limit, try this option
